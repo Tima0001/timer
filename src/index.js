b6135e533e2cb46ad2887f888b6b6ea58490c9ad
Tima0001 <microsoft.timo1@gmail.com>
2020-10-17 13:02:03 +0300

    timer

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// import css from "./css/style.css";
+// import timerRefs from './refs/timerRefs.js'
+
+// let myDate = `Jan 1 2021`
+
+// function getTimeBetween(){
+//   currentTime = Date.now()
+//   let timeBetween = +newYearTime - Date.now()
+//   let daysBefore = Math.floor(timeBetween / 1000 / 60 / 60 / 24)
+//   let hoursBefore = Math.floor(timeBetween % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ))
+//   let minutesBefore = Math.floor(timeBetween % (1000 * 60 * 60) / (1000 * 60  ))
+//   let secondsBefore = Math.floor(timeBetween % (1000 * 60) / 1000  )
+
+//   timerRefs.daysContent.textContent = daysBefore
+//   timerRefs.hoursContent.textContent = hoursBefore
+//   timerRefs.minutesContent.textContent = minutesBefore
+//   timerRefs.secondsContent.textContent = secondsBefore
+//   // console.log("hoursBefore",hoursBefore)
+// }
+
+// let interval = setInterval(getTimeBetween, 1000)
+
+import css from "./css/style.css";
+import timerRefs from './refs/timerRefs.js'
+
+let myDate = `Jan 1 2021`
+
+function getTimeBetween(date){
+  let newYearTime = new Date(date)
+  let timeBetween = +newYearTime - Date.now()
+  //       ms = ms          /  s    / min / hours /
+  let daysBefore = Math.floor(timeBetween / 1000 / 60 / 60 / 24)
+  let hoursBefore = Math.floor(timeBetween % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ))
+  let minutesBefore = Math.floor(timeBetween % (1000 * 60 * 60) / (1000 * 60  ))
+  let secondsBefore = Math.floor(timeBetween % (1000 * 60) / 1000  )
+
+  timerRefs.daysContent.textContent = daysBefore
+  timerRefs.hoursContent.textContent = hoursBefore
+  timerRefs.minsContent.textContent = minutesBefore
+  timerRefs.secondsContent.textContent = secondsBefore
+}
+
+let interval = setInterval(()=>{
+  getTimeBetween(myDate)
+}, 1000)
---
+++ src/refs/timerRefs.js
@@ -0,0 +1,7 @@
+export default {
+  article: document.getElementById('timer'),
+  daysContent: document.getElementById('daysContent'),
+  hoursContent: document.getElementById('hoursContent'),
+  minsContent: document.getElementById('minsContent'),
+  secondsContent: document.getElementById('secondsContent'),
+}
